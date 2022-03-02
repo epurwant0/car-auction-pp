4a245fba4fbfedbf2c0b3f8d9130cbe9e4154785
epurwant0 <ellis.purwanto@gmail.com>
2022-03-01 23:22:14 -0500

    revised app.py

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template
 
 # Import our psycopg2 library, which lets us connect our Flask app to our Postgres database.
 import sqlalchemy
+import os
+
+db_uri = os.environ.get('DATABASE_URL')
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
-connection_string = f"postgres:Payrol662!@localhost:5433/CarAuctionDB"
+# connection_string = f"postgres:Payrol662!@localhost:5433/CarAuctionDB"
 
 # Pass connection url to the sqlalchemy create_engine method
-engine = sqlalchemy.create_engine(f'postgresql://{connection_string}')
+engine = sqlalchemy.create_engine(db_uri)
 
 # Set route
 @app.route('/')
